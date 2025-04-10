f913189c07aeeb716f472f90542e7c8285e8d8c0
Olga <okatranis@gmail.com>
2025-04-10 13:14:25 +0300

    feat: create auth page

---
+++ todo/src/pages/auth.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import LoginForm from "../components/loginForm";
+import RegisterForm from "../components/registerForm";
+
+const Auth = () => {
+  const [isLogin, setIsLogin] = useState(true);
+
+  return (
+    <div className="auth-container">
+      <div className="flex justify-center mb-4">
+        <button
+          onClick={() => setIsLogin(true)}
+          className={`px-4 py-2 mr-2 ${isLogin ? 'bg-blue-500' : 'bg-gray-300'} text-white rounded`}
+        >
+          Login
+        </button>
+        <button
+          onClick={() => setIsLogin(false)}
+          className={`px-4 py-2 ${!isLogin ? 'bg-blue-500' : 'bg-gray-300'} text-white rounded`}
+        >
+          Register
+        </button>
+      </div>
+
+      {isLogin ? <LoginForm /> : <RegisterForm />}
+    </div>
+  );
+};
+
+export default Auth;
